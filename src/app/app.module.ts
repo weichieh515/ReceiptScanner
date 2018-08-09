f33ce0185865924c79c09464ae8953e10694f6fd
Jack <ace05152535@gmail.com>
2018-08-09 23:41:24 +0800

    add http client to app module and button for setting cloud url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -15,6 +16,7 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { ScannerProvider } from '../providers/scanner/scanner';
 import { ParserProvider } from '../providers/parser/parser';
 import { AlertProvider } from '../providers/alert/alert';
+import { ApiProvider } from '../providers/api/api';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,7 @@ import { AlertProvider } from '../providers/alert/alert';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -39,7 +42,8 @@ import { AlertProvider } from '../providers/alert/alert';
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     ScannerProvider,
     ParserProvider,
-    AlertProvider
+    AlertProvider,
+    ApiProvider
   ]
 })
 export class AppModule {}
